a53de5fde02675e839c4e850178b09e45b6b49be
AbhishekYadav-09 <abhishekyadavcode@gmail.com>
2026-06-14 15:26:22 +0530

    solve problem of Age group Categorization

---
+++ Conditional Problem/01_Problem.py
@@ -0,0 +1,12 @@
+# Age group Categorization
+
+Age = int(input("Enter The Age!"));
+
+if Age < 13:
+    print("you a child")
+elif Age > 13 and Age <19 :
+    print("Teenager")    
+elif Age > 20 and Age <59 :
+    print ("Adult")    
+else :
+    print("Senior")    
