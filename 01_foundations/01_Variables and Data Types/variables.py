40ce2198d7ee91bcd0b40da630d1131ac5d653f9
AbhishekYadav-09 <abhishekyadavcode@gmail.com>
2026-06-29 04:44:41 +0530

    feat: Python variables with examples

---
+++ 01_foundations/01_Variables and Data Types/variables.py
@@ -0,0 +1,33 @@
+# variables is container that store the value
+
+# variable_name = value
+
+name = "Abhishek Yadav"
+age = 20
+hight = 5.6
+
+print(name)
+print(age)
+print(hight)
+# variables only hold refresh
+
+# Multiple Variables
+a=10
+b=20
+c=30
+
+print(a)
+print(b)
+print(c)
+
+# Multiple Assignment
+
+raju, kaju, bhalu = 88, 99, 999
+
+print(raju, bhalu)
+
+# Same Value
+
+x = y = z = 100
+
+print(x, y , z)
