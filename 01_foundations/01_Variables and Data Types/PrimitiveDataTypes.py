b9cfaf806f1d5d68b97ff806da6d21f23ad852c3
AbhishekYadav-09 <abhishekyadavcode@gmail.com>
2026-06-29 04:56:52 +0530

    feat: primitiveDataType in python

---
+++ 01_foundations/01_Variables and Data Types/PrimitiveDataTypes.py
@@ -0,0 +1,33 @@
+# int
+
+x = 100
+y = 200
+
+print(x+y)
+
+# 2.float
+
+pi = 3.14
+print(type(pi))
+
+# 3.bool
+
+is_admin = True
+is_login = False
+
+print(type(is_admin))
+
+# str
+name = "Abhishek"
+
+print(type(name))
+print(name[0])
+print(name[1])
+print(len(name))
+
+# complex
+
+x = 5 + 3j
+
+print(type(x))
+
