48d890179bb4ce97a2ed06364fc4beafeb8625ba
AbhishekYadav-09 <abhishekyadavcode@gmail.com>
2026-06-29 07:39:26 +0530

    print in python

---
+++ 01_foundations/02_Input and Output/print.py
@@ -0,0 +1,20 @@
+# print() ka use output screen par dikhane ke liye hota hai.
+print("Hello World")
+
+# Printing Multiple Values
+
+name = "Abhishek Yadav"
+age = 21
+
+print(name, age)
+
+# Multiple values ke beech separator change kar sakte ho.
+
+print("2026", "07", "01", sep="-")
+print("python", "C++", "Java", sep="|")
+
+# end Parameter
+# Default me print() ke baad next line aa jati hai.
+
+print("Hello", end=" ")
+print("World")
