ba31b4bc88c8274fb5ec85469278a073f6d1b615
AbhishekYadav-09 <abhishekyadavcode@gmail.com>
2026-06-29 08:03:38 +0530

    Escape Charecter in python

---
+++ 01_foundations/02_Input and Output/Escape_Characters.py
@@ -0,0 +1,23 @@
+# Escape character backslash (\) se start hota hai.
+
+print("Hello\nWorld")
+
+# Tab
+
+print("Python\tJava\tC++")
+
+# Backslash
+
+print("C:\\name\\abhi")
+
+# Double Quote
+print("He said \"Hello\"")
+
+# Single Quote
+print('It\'s Python')
+
+# Raw String
+# Windows paths ke liye useful.
+
+path = r"C:\Users\Abhishek\Desktop"
+print(path)
