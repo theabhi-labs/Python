72a09119c908ebf38812978454badddc85e3d944
AbhishekYadav-09 <abhishekyadavcode@gmail.com>
2026-07-02 03:44:39 +0530

    for loop in python

---
+++ 01_foundations/05_Loops/01_for_Loop.py
@@ -0,0 +1,23 @@
+# Jab hame pata ho kitni baar loop chalana hai.
+
+for i in range(11):
+    print(i)
+
+# Start aur End dena
+for i in range(1, 7):
+    print(i)
+
+# Step dena
+for i in range(0, 11, 5):
+    print(i)
+
+# List par loop
+num = [10, 20, 30, 40, 50, 60]    
+for i in num:
+    print(i)
+
+# String par loop
+name = "Abhishek"
+
+for ch in name:
+    print(ch)     
