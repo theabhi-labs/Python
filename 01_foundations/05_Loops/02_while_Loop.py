11a1382f33d29e362791b5e4d2d70cd3d01b775c
AbhishekYadav-09 <abhishekyadavcode@gmail.com>
2026-07-02 03:53:16 +0530

    while loop in python

---
+++ 01_foundations/05_Loops/02_while_Loop.py
@@ -0,0 +1,28 @@
+# Jab tak condition True rahegi tab tak loop chalega.
+
+i = 1
+
+while i <= 5:
+    print(i)
+    i += 1
+
+# Infinite Loop
+# while True:
+    # print("Hello")    
+
+
+# break
+for i in range(10):
+    if i == 5:
+        break
+
+    print(i)    
+
+# continue
+# Current iteration skip kar deta hai.    
+
+for i in range(6):
+    if i == 3:
+        continue
+    print(i)
+
